2bd7197ddeb8bcbc0d0445c612f85ec196e9b8c9
faridreactheme-beep <farid.reactheme@gmail.com>
2025-10-15 18:15:15 +0600

    code optimize

--- easy-elements-pro.php
+++ easy-elements-pro.php
@@ -87,39 +87,196 @@ function easy_elements_pro_plugin_init() {
     }
 
     // Hook into Elementor widgets registration
+   
     add_action( 'elementor/widgets/register', function( $widgets_manager ) {
-        $pro_widgets = [           
-            'clients_logo_slider'   => [ 'class' => '\Easyel_Clients_Logo_Slider__Widget',  'file' => EASYELEMENTS_PRO_PATH . '/widgets/clients-logo-slider/logo.php','tab' => 'widget' ],
-            'image_carousel'        => [ 'class' => '\Easyel_Image_Carousel__Widget',       'file' => EASYELEMENTS_PRO_PATH . '/widgets/image-carousel/carousel.php','tab' => 'widget' ],
-            'tab_advance'           => [ 'class' => '\Easyel_Advance_Tab_Widget',           'file' => EASYELEMENTS_PRO_PATH . '/widgets/tab-advance/tab.php','tab' => 'widget' ],
-            'testimonials_slider'   => [ 'class' => '\Easyel_Testimonials_Slider__Widget',  'file' => EASYELEMENTS_PRO_PATH . '/widgets/testimonials-slider/testimonials.php','tab' => 'widget' ],
-            'process_grid'          => [ 'class' => '\Easyel_Process_Widget',               'file' => EASYELEMENTS_PRO_PATH . '/widgets/process-grid/process-grid.php','tab' => 'widget' ],
-            'process_slider'        => [ 'class' => '\Easyel_Process_Slider_Widget',        'file' => EASYELEMENTS_PRO_PATH . '/widgets/process-slider/process-slider.php','tab' => 'widget' ],
-            'team_slider'           => [ 'class' => '\Easyel_Team_Slider__Widget',          'file' => EASYELEMENTS_PRO_PATH . '/widgets/team-slider/team-slider.php','tab' => 'widget' ],
-            'icon_box_slider'       => [ 'class' => '\Easyel_Icon_Box_Slider__Widget',      'file' => EASYELEMENTS_PRO_PATH . '/widgets/icon-box-slider/icon-box-slider.php','tab' => 'widget' ],
-            'post_slider'           => [ 'class' => '\Easyel_Blog_Slider__Widget',          'file' => EASYELEMENTS_PRO_PATH . '/widgets/blog-slider/blog-slider.php' ,'tab' => 'widget'],
-            'process_list'           => [ 'class' => '\Easyel_Process_list_Widget',          'file' => EASYELEMENTS_PRO_PATH . '/widgets/process-list/process-list.php','tab' => 'widget' ],
-            'marquee_logo'           => [ 'class' => '\Easyel_Marquee_Logo__Widget',         'file' => EASYELEMENTS_PRO_PATH . '/widgets/marquee-logo/marquee.php' ,'tab' => 'widget'],
-            'archive_post'           => [ 'class' => '\Easyel_Archive_Post__Widget',          'file' => EASYELEMENTS_PRO_PATH . '/widgets/archive-post/archive-post.php','tab' => 'widget' ],
-            'excerpt'                => [ 'class' => '\Easyel_Excerpt_Widget',               'file' => EASYELEMENTS_PRO_PATH . '/widgets/excerpt/excerpt.php','tab' => 'widget' ],
-            'post_title'            => [ 'class' => '\Easyel_Post_Title_Widget',             'file' => EASYELEMENTS_PRO_PATH . '/widgets/post-title/post-title.php','tab' => 'widget' ],
-            'post_content'           => [ 'class' => '\Easyel_Post_Content_Widget',          'file' => EASYELEMENTS_PRO_PATH . '/widgets/post-content/post-content.php','tab' => 'widget' ],
-            'featured_image'          => [ 'class' => '\Easyel_Featured_Image_Widget',        'file' => EASYELEMENTS_PRO_PATH . '/widgets/featured-image/featured-image.php' ,'tab' => 'widget'],
-            'related_post'            => [ 'class' => '\Easyel_Related_Post_Widget',           'file' => EASYELEMENTS_PRO_PATH . '/widgets/related-post/related.php','tab' => 'widget' ],
-            'post_meta'               => [ 'class' => '\Easyel_Post_Meta_Widget',             'file' => EASYELEMENTS_PRO_PATH . '/widgets/post-meta/post-meta.php','tab' => 'widget' ],
-            'post_pagination'         => [ 'class' => '\Easyel_Post_Pagination_Widget',        'file' => EASYELEMENTS_PRO_PATH . '/widgets/post-pagination/post-pagination.php','tab' => 'widget' ],
-            'post_comments'           => [ 'class' => '\Easyel_Post_Comments_Widget',          'file' => EASYELEMENTS_PRO_PATH . '/widgets/post-comments/post-comments.php','tab' => 'widget' ],
-            'easy_slider'             => [ 'class' => '\Easyel_Easy_Slider_Widget',            'file' => EASYELEMENTS_PRO_PATH . '/widgets/easy-slider/easy-slider.php' ,'tab' => 'widget'],
-            'image_accordion'         => [ 'class' => '\Easyel_Image_Accordion_Widget',        'file' => EASYELEMENTS_PRO_PATH . '/widgets/image-accordion/image-accordion.php','tab' => 'widget' ],
-            'featured_project'         => [ 'class' => '\Easyel_Featured_Project_Widget',        'file' => EASYELEMENTS_PRO_PATH . '/widgets/feature-project/feature-project.php','tab' => 'widget' ],
-            'advance_button'         => [ 'class' => '\Easyel_Advance_Button_Widget',        'file' => EASYELEMENTS_PRO_PATH . '/widgets/advance-button/button.php','tab' => 'widget' ],
-            'hr_image_scroll'         => [ 'class' => '\Easyel_Hr_Image_Scroll_Widget',        'file' => EASYELEMENTS_PRO_PATH . '/widgets/horizontal-image-scroll/horizontal-image-scroll.php','tab' => 'widget' ],
-            'post_tags'         => [ 'class' => '\Easyel_Post_Tags_Widget',        'file' => EASYELEMENTS_PRO_PATH . '/widgets/post-tag/post-tag.php','tab' => 'widget' ],
-            'post_author'         => [ 'class' => '\Easyel_Post_Author_Info_Widget',        'file' => EASYELEMENTS_PRO_PATH . '/widgets/post-author/post-author.php','tab' => 'widget' ],
-            'timeline_slider'         => [ 'class' => '\Easyel_Timeline_Slider_Widget',        'file' => EASYELEMENTS_PRO_PATH . '/widgets/timeline-slider/timeline.php','tab' => 'widget' ],
-            'enable_image_hover_effect'         => [ 'class' => '\Easyel_image_hover_effect__Widget',        'file' => EASYELEMENTS_PRO_PATH . '/widgets/image-hover-effect/image-hover-effect.php','tab' => 'widget' ],
-            'image_gallery_filter'         => [ 'class' => '\Easyel__Gallery_Pro_Widget',        'file' => EASYELEMENTS_PRO_PATH . '/widgets/gallery-filter/gallery-filter.php','tab' => 'widget' ],
-            'portfolio_pro'         => [ 'class' => '\Easyel_Portfolio_Pro_Widget',        'file' => EASYELEMENTS_PRO_PATH . '/widgets/portfolio/portfolio.php','tab' => 'widget' ],
+
+        $pro_widgets = [
+
+            'clients_logo_slider' => [
+                'class' => '\Easyel_Clients_Logo_Slider__Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/clients-logo-slider/logo.php',
+                'tab'   => 'widget'
+            ],
+
+            'image_carousel' => [
+                'class' => '\Easyel_Image_Carousel__Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/image-carousel/carousel.php',
+                'tab'   => 'widget'
+            ],
+
+            'tab_advance' => [
+                'class' => '\Easyel_Advance_Tab_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/tab-advance/tab.php',
+                'tab'   => 'widget'
+            ],
+
+            'testimonials_slider' => [
+                'class' => '\Easyel_Testimonials_Slider__Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/testimonials-slider/testimonials.php',
+                'tab'   => 'widget'
+            ],
+
+            'process_grid' => [
+                'class' => '\Easyel_Process_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/process-grid/process-grid.php',
+                'tab'   => 'widget'
+            ],
+
+            'process_slider' => [
+                'class' => '\Easyel_Process_Slider_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/process-slider/process-slider.php',
+                'tab'   => 'widget'
+            ],
+
+            'team_slider' => [
+                'class' => '\Easyel_Team_Slider__Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/team-slider/team-slider.php',
+                'tab'   => 'widget'
+            ],
+
+            'icon_box_slider' => [
+                'class' => '\Easyel_Icon_Box_Slider__Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/icon-box-slider/icon-box-slider.php',
+                'tab'   => 'widget'
+            ],
+
+            'post_slider' => [
+                'class' => '\Easyel_Blog_Slider__Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/blog-slider/blog-slider.php',
+                'tab'   => 'widget'
+            ],
+
+            'process_list' => [
+                'class' => '\Easyel_Process_list_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/process-list/process-list.php',
+                'tab'   => 'widget'
+            ],
+
+            'marquee_logo' => [
+                'class' => '\Easyel_Marquee_Logo__Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/marquee-logo/marquee.php',
+                'tab'   => 'widget'
+            ],
+
+            'archive_post' => [
+                'class' => '\Easyel_Archive_Post__Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/archive-post/archive-post.php',
+                'tab'   => 'widget'
+            ],
+
+            'excerpt' => [
+                'class' => '\Easyel_Excerpt_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/excerpt/excerpt.php',
+                'tab'   => 'widget'
+            ],
+
+            'post_title' => [
+                'class' => '\Easyel_Post_Title_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/post-title/post-title.php',
+                'tab'   => 'widget'
+            ],
+
+            'post_content' => [
+                'class' => '\Easyel_Post_Content_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/post-content/post-content.php',
+                'tab'   => 'widget'
+            ],
+
+            'featured_image' => [
+                'class' => '\Easyel_Featured_Image_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/featured-image/featured-image.php',
+                'tab'   => 'widget'
+            ],
+
+            'related_post' => [
+                'class' => '\Easyel_Related_Post_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/related-post/related.php',
+                'tab'   => 'widget'
+            ],
+
+            'post_meta' => [
+                'class' => '\Easyel_Post_Meta_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/post-meta/post-meta.php',
+                'tab'   => 'widget'
+            ],
+
+            'post_pagination' => [
+                'class' => '\Easyel_Post_Pagination_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/post-pagination/post-pagination.php',
+                'tab'   => 'widget'
+            ],
+
+            'post_comments' => [
+                'class' => '\Easyel_Post_Comments_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/post-comments/post-comments.php',
+                'tab'   => 'widget'
+            ],
+
+            'easy_slider' => [
+                'class' => '\Easyel_Easy_Slider_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/easy-slider/easy-slider.php',
+                'tab'   => 'widget'
+            ],
+
+            'image_accordion' => [
+                'class' => '\Easyel_Image_Accordion_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/image-accordion/image-accordion.php',
+                'tab'   => 'widget'
+            ],
+
+            'featured_project' => [
+                'class' => '\Easyel_Featured_Project_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/feature-project/feature-project.php',
+                'tab'   => 'widget'
+            ],
+
+            'advance_button' => [
+                'class' => '\Easyel_Advance_Button_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/advance-button/button.php',
+                'tab'   => 'widget'
+            ],
+
+            'hr_image_scroll' => [
+                'class' => '\Easyel_Hr_Image_Scroll_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/horizontal-image-scroll/horizontal-image-scroll.php',
+                'tab'   => 'widget'
+            ],
+
+            'post_tags' => [
+                'class' => '\Easyel_Post_Tags_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/post-tag/post-tag.php',
+                'tab'   => 'widget'
+            ],
+
+            'post_author' => [
+                'class' => '\Easyel_Post_Author_Info_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/post-author/post-author.php',
+                'tab'   => 'widget'
+            ],
+
+            'timeline_slider' => [
+                'class' => '\Easyel_Timeline_Slider_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/timeline-slider/timeline.php',
+                'tab'   => 'widget'
+            ],
+
+            'enable_image_hover_effect' => [
+                'class' => '\Easyel_image_hover_effect__Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/image-hover-effect/image-hover-effect.php',
+                'tab'   => 'widget'
+            ],
+
+            'image_gallery_filter' => [
+                'class' => '\Easyel__Gallery_Pro_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/gallery-filter/gallery-filter.php',
+                'tab'   => 'widget'
+            ],
+
+            'portfolio_pro' => [
+                'class' => '\Easyel_Portfolio_Pro_Widget',
+                'file'  => EASYELEMENTS_PRO_PATH . '/widgets/portfolio/portfolio.php',
+                'tab'   => 'widget'
+            ],
         ];
 
         $extensions_settings = get_option( 'easy_element_extensions', [] );
@@ -129,10 +286,10 @@ function easy_elements_pro_plugin_init() {
             $is_enabled = false;
 
             if ( $widget['tab'] === 'extensions' ) {
-                $is_enabled = isset( $extensions_settings[$key] ) && $extensions_settings[$key] == 1;
+                $is_enabled = isset( $extensions_settings[ $key ] ) && $extensions_settings[ $key ] == 1;
             } else {
                 $option_name = 'easy_element_' . $widget['tab'] . '_' . $key;
-                $is_enabled = get_option( $option_name, '1' ) == '1';
+                $is_enabled  = get_option( $option_name, '1' ) == '1';
             }
 
             if ( ! $is_enabled ) {
@@ -146,7 +303,9 @@ function easy_elements_pro_plugin_init() {
                 }
             }
         }
+
     });
+
 }
 
 class Easy_Elements_Pro {
